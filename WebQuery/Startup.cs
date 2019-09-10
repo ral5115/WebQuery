ddd63587a9d57e0e7a14b89651e5202e04637267
andres lopez <darkandres22@gmail.com>
2019-09-10 17:13:31 -0500

    nuguet Mysql

--- WebQuery/Startup.cs
+++ WebQuery/Startup.cs
@@ -32,6 +32,7 @@ namespace WebQuery
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //services.AddDbContext<ApplicationDBContext>
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
