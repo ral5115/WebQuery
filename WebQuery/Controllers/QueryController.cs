bd982f1b601b28244d881c40194389ee61900c42
andres lopez <ralopezcastano51@gmail.com>
2020-07-17 14:26:55 -0500

    implementacion de mysql y clase sql - 20200717

---
+++ WebQuery/Controllers/QueryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebQuery.Controllers
+{
+    public class QueryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebQuery/DAO/SQLTransaction.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebQuery.DAO
+{
+    public class SQLTransaction
+    {
+        public static IConfiguration Configuration { get; set; }
+        public DataSet GetData()
+        {
+
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            DataSet result = new DataSet();
+            Configuration = builder.Build();
+            MySqlConnection connection = new MySqlConnection(Configuration.GetConnectionString("DefaultConnection"));
+            MySqlCommand command = new MySqlCommand("SELECT * FROM BD_VINCORTE.cache;", connection);
+            command.CommandType = CommandType.Text;
+            MySqlDataAdapter daData = new MySqlDataAdapter();
+
+
+            try
+            {
+                daData.SelectCommand = command;
+                daData.Fill(result);
+                
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            finally
+            {
+                command.Connection.Close();
+            }
+
+
+
+            return result;
+        }
+    }
+}
